984a49462deb44c2f482fdd51bcf433a2c380297
wcchang1382 <wcc1213@gmail.com>
2017-06-19 10:44:52 -0700

    Team1 pet object (#4)
    * add js folder and empty scripts.js file
    
    * create pet object and methods to get and set properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Business Logic
-//fake array called fakeCenter
-var fakeCenter = [
+
+var Center = [
   {name: "dog1", available: true, type: "dog"},
   {name: "dog2", available: true, type: "dog"},
   {name: "dog3", available: true, type: "dog"},
@@ -10,6 +10,38 @@ var fakeCenter = [
   {name: "Ranger", available: false, type: "cat"},
   {name: "Dallas", available: false, type: "cat"},
   {name: "cat1", available: true, type: "cat"},];
+
+function Pet (name,type) {
+	this.name = name;
+  this.type = type;
+  this.isAvailable = true;
+}
+
+Pet.prototype.getName = function () {
+	return this.name;
+}
+
+Pet.prototype.setName = function (name) {
+	this.name = name;
+}
+
+Pet.prototype.getType = function () {
+	return this.type;
+}
+
+Pet.prototype.setType = function (type) {
+	this.type = type;
+}
+
+Pet.prototype.getIsAvailable = function () {
+	return this.isAvailable;
+}
+
+Pet.prototype.setIsAvailable = function (isAvailable) {
+	this.isAvailable = isAvailable;
+} //must be true or false.
+
+
 //User Interface
 $(function() {
 //Loop through the Center object
@@ -20,3 +52,4 @@ fakeCenter.forEach(function(pet) {
 //each list item has a name,
 //each name will be a link to details of the pet
 });
+
