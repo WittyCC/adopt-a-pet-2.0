07dff881de287c79b002b6d404662d99ada69437
ag, ak and sz <ak@>
2017-06-19 11:52:42 -0700

    delete fake arrays and fix the loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,5 @@
 //Business Logic
 
-var Center = [
-  {name: "dog1", available: true, type: "dog"},
-  {name: "dog2", available: true, type: "dog"},
-  {name: "dog3", available: true, type: "dog"},
-  {name: "dog4", available: true, type: "dog"},
-  {name: "dog5", available: true, type: "dog"},
-  {name: "dog6", available: true, type: "dog"},
-  {name: "Ranger", available: false, type: "cat"},
-  {name: "Dallas", available: false, type: "cat"},
-  {name: "cat1", available: true, type: "cat"}];
-
-  var fakeCenter = [
-    {name: "dog1", isAvailable: true, type: "dog"},
-    {name: "dog2", isAvailable: true, type: "dog"},
-    {name: "dog3", isAvailable: true, type: "dog"},
-    {name: "dog4", isAvailable: true, type: "dog"},
-    {name: "dog5", isAvailable: true, type: "dog"},
-    {name: "dog6", isAvailable: true, type: "dog"},
-    {name: "Ranger", isAvailable: false, type: "cat"},
-    {name: "Dallas", isAvailable: false, type: "cat"},
-    {name: "cat1", isAvailable: true, type: "cat"}];
-
 function Pet (name,type) {
 	this.name = name;
   this.type = type;
@@ -51,6 +29,7 @@ Pet.prototype.getIsAvailable = function () {
 Pet.prototype.setIsAvailable = function (isAvailable) {
 	this.isAvailable = isAvailable;
 } //must be true or false.
+var myCenter = [];
 
 var pet1 = new Pet ("Bruce", "dog");
 pet1.setIsAvailable(true);
@@ -88,22 +67,10 @@ var pet9 = new Pet ("Velcro", "cat");
 pet9.setIsAvailable(true);
 myCenter.push(pet9);
 
-var myCenter = [];
-
-var tempString ="";
-
-for (var i=0; i<myCenter.length; i++) {
-	tempString += myCenter[i].name;
-}
-
-alert (tempString);
-
-
-
 //User Interface
 $(function() {
   //Loop through the Center object//create a list item for each pet
-  fakeCenter.forEach(function(pet) {
+  myCenter.forEach(function(pet) {
     $("#petList").append("<li class='petClass'>" + pet.name + "</li>");
     //each name will be a link to details of the pet
     $(".petClass").last().click(function(){
@@ -115,4 +82,3 @@ $(function() {
   });
 
 });
-
