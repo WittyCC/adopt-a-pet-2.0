b32525877c3b1459e6eaf05edf2ca4512ae668ad
ag, ak and sz <sz@>
2017-06-19 10:29:46 -0700

    add fake center and placeholder for the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 //Business Logic
 //fake array called fakeCenter
+var fakeCenter = [
+  {name: "dog1", available: true, type: "dog"},
+  {name: "dog2", available: true, type: "dog"},
+  {name: "dog3", available: true, type: "dog"},
+  {name: "dog4", available: true, type: "dog"},
+  {name: "dog5", available: true, type: "dog"},
+  {name: "dog6", available: true, type: "dog"},
+  {name: "Ranger", available: false, type: "cat"},
+  {name: "Dallas", available: false, type: "cat"},
+  {name: "cat1", available: true, type: "cat"},];
 //User Interface
 $(function() {
 //Loop through the Center object
+fakeCenter.forEach(function(pet) {
+
+})
 //create a list item for each pet
 //each list item has a name,
 //each name will be a link to details of the pet
