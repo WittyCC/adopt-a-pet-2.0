1767d034f1a36af566d13622d7d2e1b0a44b948e
ag, ak and sz <ak@>
2017-06-19 10:19:12 -0700

    add jQuery, links to jQuery and JS, add basic layout

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//Business Logic
+//fake array called fakeCenter
+//User Interface
+$(function() {
+//Loop through the Center object
+//create a list item for each pet
+//each list item has a name,
+//each name will be a link to details of the pet
+});
